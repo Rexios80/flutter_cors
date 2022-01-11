4671cbf8925af5aaa95350a55c4c9616d9c68ca2
Rexios <rexios@rexios.dev>
2022-01-10 20:48:54 -0500

    Let's try this

--- bin/flutter_cors.dart
+++ bin/flutter_cors.dart
@@ -61,7 +61,7 @@ void disable(String flutterPath) {
   }
   final chromeDartContentsWithWebSecurity = chromeDartContents.replaceFirst(
     "'--disable-extensions',",
-    "'--disable-extensions',$newLine      '--disable-web-security',",
+    "'--disable-extensions',\n      '--disable-web-security',",
   );
 
   // Write the new contents to the file
